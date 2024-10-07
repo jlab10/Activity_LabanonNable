6db4145f2801efe37db33fca239a3004bd4f88dc
Nable <jakemarson03@gmail.com>
2024-10-07 18:36:13 +0800

    giusab na version 1.4

--- Activity_LabanonNable/src/activity_labanonnable/Activity_LabanonNable.java
+++ Activity_LabanonNable/src/activity_labanonnable/Activity_LabanonNable.java
@@ -4,106 +4,84 @@ import java.util.Scanner;
 
 
 public class Activity_LabanonNable {
-
-    static String name;
     static double result;
-    static int nums, age, grade;
+    static int nums;
 
-    
     public static void main(String[] args) throws InterruptedException {
         
         System.out.println("================================================================================ HelloWorld ================================================================================");
         Scanner sc=new Scanner(System.in);
         boolean Loop=true;
-        System.out.println("[1]Calculate Numbers\n[2]Add Student Information\nEnter: ");
-        int choose=sc.nextInt();
-        
-        if(choose==2){
-            StudentInformation information = new StudentInformation();
-            information.name(name);
-            information.age(age);
-            information.grade(grade);
-            System.out.println("[STUDENT INFORMATION ADDED]");
-            System.out.println("Name: "+name+"\nAge: "+age+"\nGrade Level: "+grade);            
-        }
-        else if(choose==1) {
         
         while (Loop==true){
-            System.out.print("INPUT AT LEAST 2 NUMBERS\nHow many numbers do you want to input? ");
-            nums = sc.nextInt();
+            System.out.print("[1]Calculate Numbers\n[2]Add Student Information\nEnter: ");
+            int choose=sc.nextInt();
+            if(choose==2){
+
+                System.out.print("Enter the number of students you want to input\nEnter: ");
+                int amount = sc.nextInt();
+                if(amount>=1){
+                
+                    String name[]=new String[amount];
+                    int grade[]=new int [amount];
+                    int age[]=new int [amount];
+                    int numStudents[]=new int [amount];
+
+                    for(int j=0;j<numStudents.length;j++){                 
+                        System.out.print("\nName: ");
+                        sc.nextLine();
+                        String studentName=sc.nextLine();
+                        name[j]=studentName;
+                    
+                        System.out.print("Age: ");
+                        int studentAge=sc.nextInt();
+                        age[j]=studentAge;
+                    
+                        System.out.print("Year-Level: ");
+                        int studentGrade=sc.nextInt();
+                        grade[j]=studentGrade;
+                    }
+                    System.out.print("\n\nLIST OF STUDENTS");
+                    for(int j=0;j<numStudents.length;j++){
+                        String nameOutput;
+                        int ageOutput;
+                        int gradeOutput;
+                    
+                        nameOutput=name[j];
+                        ageOutput=age[j];
+                        gradeOutput=grade[j];
+                    
+                        System.out.print("\nName: "+nameOutput+"\nAge: "+ageOutput+"\nYear-Level: "+gradeOutput+"\n");
+                    }
+                    System.out.println("\nDo you want to perform another action?");
+                    sc.nextLine();
+                    System.out.println("Choose:[Y] Yes. [N] No.");
+                    String calculate=sc.nextLine();
             
-            if(nums<=1){
-                System.out.print("INVALID OUTPUT\nClosing Program");
-                Thread.sleep(500);
-                System.out.print(".");
-                Thread.sleep(700);
-                System.out.print(".");
-                Thread.sleep(700);
-                System.out.print(".\n");
-                System.out.println("Have a nice day");
-                Loop=false;
-            }else{
-                double numbers[]=new double [nums];
-                for(int i=0;i<numbers.length;i++){
-                    System.out.print("Enter number: ");
-                    double numInputs=sc.nextDouble();
-                    numbers[i]=numInputs;
-                }
-                System.out.print("Choose an operation:\n[1] Addition\n[2] Subtraction\n[3] Division\n[4] Multiplication\n[5] Mean\nEnter:");
-                int operation=sc.nextInt();
-                switch (operation){
-                    case 1:
-                        result=numbers[0];
-                        for(int j=1;j<numbers.length;j++){
-                            result+=numbers[j];
-                        }
-                        System.out.println("Total: "+result);
-                        break;        
-                    case 2:
-                        result=numbers[0];
-                        for(int j=1;j<numbers.length;j++){
-                            result-=numbers[j];
-                        }
-                        System.out.println("Difference: "+result);
-                        break;
-                    case 3:
-                        result = numbers[0];
-                        for(int j=1;j<numbers.length;j++){
-                            result/=numbers[j];
-                        }
-                        System.out.println("Quotient: "+result);
-                        break;
-                    case 4:
-                        result = numbers[0];
-                        for(int j=1;j<numbers.length;j++){
-                            result*=numbers[j];
-                        }
-                        System.out.println("Product: "+result);
-                        break;
-                    case 5:
-                        NewClass newClass = new NewClass();
-                        newClass.mean(numbers);
-                        System.out.println("");
-                        break;
-                }
-                System.out.println("\nDo you want to calculate another numbers?");
-                sc.nextLine();
-                System.out.println("Choose:[Y] Yes. [N] No.");
-                String calculate=sc.nextLine();
-            
-                if (calculate.equalsIgnoreCase("n")){
-                    System.out.print("Closing Program");
-                    Thread.sleep(500);
-                    System.out.print(".");
-                    Thread.sleep(700);
-                    System.out.print(".");
-                    Thread.sleep(700);
-                    System.out.print(".\n");
-                    System.out.println("Have a nice day");
-                    Loop=false;
-                }
-                else if(calculate.equalsIgnoreCase("y")){
-                    System.out.print("\n");
+                    if (calculate.equalsIgnoreCase("n")){
+                        System.out.print("Closing Program");
+                        Thread.sleep(500);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".\n");
+                        System.out.println("Have a nice day");
+                        Loop=false;
+                    }
+                    else if(calculate.equalsIgnoreCase("y")){
+                        System.out.print("\n");
+                    }else{
+                        System.out.print("INVALID OUTPUT\nClosing Program");
+                        Thread.sleep(500);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".\n");
+                        System.out.println("Have a nice day");
+                        Loop=false;
+                    }
                 }else{
                     System.out.print("INVALID OUTPUT\nClosing Program");
                     Thread.sleep(500);
@@ -114,12 +92,13 @@ public class Activity_LabanonNable {
                     System.out.print(".\n");
                     System.out.println("Have a nice day");
                     Loop=false;
-                }
-            }         
-        }
-    }
-        else{
-            System.out.print("INVALID OUTPUT\nClosing Program");
+                }   
+            }else if(choose==1) {
+                System.out.print("INPUT AT LEAST 2 NUMBERS\nHow many numbers do you want to input? ");
+                nums = sc.nextInt();
+            
+                if(nums<=1){
+                    System.out.print("INVALID OUTPUT\nClosing Program");
                     Thread.sleep(500);
                     System.out.print(".");
                     Thread.sleep(700);
@@ -127,6 +106,91 @@ public class Activity_LabanonNable {
                     Thread.sleep(700);
                     System.out.print(".\n");
                     System.out.println("Have a nice day");
+                    Loop=false;
+                }else{
+                    double numbers[]=new double [nums];
+                    for(int i=0;i<numbers.length;i++){
+                        System.out.print("Enter number: ");
+                        double numInputs=sc.nextDouble();
+                        numbers[i]=numInputs;
+                    }
+                    System.out.print("Choose an operation:\n[1] Addition\n[2] Subtraction\n[3] Division\n[4] Multiplication\n[5] Mean\nEnter:");
+                    int operation=sc.nextInt();
+                    switch (operation){
+                        case 1:
+                            result=numbers[0];
+                            for(int j=1;j<numbers.length;j++){
+                                result+=numbers[j];
+                            }
+                            System.out.println("Total: "+result);
+                            break;        
+                        case 2:
+                            result=numbers[0];
+                            for(int j=1;j<numbers.length;j++){
+                                result-=numbers[j];
+                            }
+                            System.out.println("Difference: "+result);
+                            break;
+                        case 3:
+                            result = numbers[0];
+                            for(int j=1;j<numbers.length;j++){
+                                result/=numbers[j];
+                            }
+                            System.out.println("Quotient: "+result);
+                            break;
+                        case 4:
+                            result = numbers[0];
+                            for(int j=1;j<numbers.length;j++){
+                                result*=numbers[j];
+                            }
+                            System.out.println("Product: "+result);
+                            break;
+                        case 5:
+                            NewClass newClass = new NewClass();
+                            newClass.mean(numbers);
+                            System.out.println("");
+                            break;
+                    }
+                    System.out.println("\nDo you want to perform another action?");
+                    sc.nextLine();
+                    System.out.println("Choose:[Y] Yes. [N] No.");
+                    String calculate=sc.nextLine();
+            
+                    if (calculate.equalsIgnoreCase("n")){
+                        System.out.print("Closing Program");
+                        Thread.sleep(500);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".\n");
+                        System.out.println("Have a nice day");
+                        Loop=false;
+                    }
+                    else if(calculate.equalsIgnoreCase("y")){
+                        System.out.print("\n");
+                    }else{
+                        System.out.print("INVALID OUTPUT\nClosing Program");
+                        Thread.sleep(500);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".");
+                        Thread.sleep(700);
+                        System.out.print(".\n");
+                        System.out.println("Have a nice day");
+                        Loop=false;
+                    }
+                }         
+            }else{
+                System.out.print("INVALID OUTPUT\nClosing Program");
+                Thread.sleep(500);
+                System.out.print(".");
+                Thread.sleep(700);
+                System.out.print(".");
+                Thread.sleep(700);
+                System.out.print(".\n");
+                System.out.println("Have a nice day");
+            }
         }
     }
 }
--- Activity_LabanonNable/src/activity_labanonnable/StudentInformation.java
+++ Activity_LabanonNable/src/activity_labanonnable/StudentInformation.java
@@ -1,8 +1,5 @@
 package activity_labanonnable;
 
-import static activity_labanonnable.Activity_LabanonNable.name;
-import static activity_labanonnable.Activity_LabanonNable.age;
-import static activity_labanonnable.Activity_LabanonNable.grade;
 import java.util.Scanner;
 
 public class StudentInformation {
@@ -10,18 +7,12 @@ public class StudentInformation {
     Scanner sc = new Scanner(System.in);
     
     public void name(String names){
-        System.out.print("Enter Name: ");
-            names = name;
-            name = sc.nextLine();     
+    
     }
     public void age(int ages){
-        System.out.print("Enter age: ");
-        ages = age;
-        age = sc.nextInt();
+       
     }
     public void grade(int grades){
-        System.out.print("Enter Grade Level: ");
-        grades = grade;
-        grade = sc.nextInt();
+
     }
 }
